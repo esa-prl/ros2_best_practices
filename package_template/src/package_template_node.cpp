577e672f4a3ab21ba82d61833245b4a5843c34c8
Maximilian Ehrhardt <max.ehrhardt@hotmail.de>
2020-09-09 17:31:28 +0200

    Add main() function to template

--- package_template/src/package_template_node.cpp
+++ package_template/src/package_template_node.cpp
@@ -4,3 +4,12 @@ PackageTemplateNode::PackageTemplateNode(rclcpp::NodeOptions options, std::strin
     : Node(node_name)
 {
 }
+
+int main(int argc, char *argv[])
+{
+    rclcpp::NodeOptions options;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<PackageTemplateNode>(options, "package_template_node"));
+    rclcpp::shutdown();
+    return 0;
+}
