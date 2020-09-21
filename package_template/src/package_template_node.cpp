395e24a9a72b79243259a2448574e32e9ac68324
maxehrhardt <max.ehrhardt@hotmail.de>
2020-09-21 16:53:27 +0200

    Update package_template/src/package_template_node.cpp
    Co-authored-by: Tim Wiese <tim.wiese@esa.int>

--- package_template/src/package_template_node.cpp
+++ package_template/src/package_template_node.cpp
@@ -1,15 +1,14 @@
 #include "package_template/package_template_node.hpp"
 
-PackageTemplateNode::PackageTemplateNode(rclcpp::NodeOptions options, std::string node_name)
-    : Node(node_name)
+PackageTemplateNode::PackageTemplateNode()
+    : Node("package_template_node")
 {
 }
 
 int main(int argc, char *argv[])
 {
-    rclcpp::NodeOptions options;
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<PackageTemplateNode>(options, "package_template_node"));
+    rclcpp::spin(std::make_shared<PackageTemplateNode>());
     rclcpp::shutdown();
     return 0;
 }
